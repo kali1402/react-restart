2287478a87b88875fdbd683fa423add493857323
kali1402 <amcopeew68@naver.com>
2020-05-27 13:04:50 +0900

    react-restart

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  const name = '김현준';
+  const age = '19'
+  const style = {
+    fontSize: 24,
+    color: "blue"
+  }
+  return (
+    <Fragment>
+      <div style={style}>{name}</div>
+      <div>{age}</div>
+    </Fragment>
+  );
+}
+
+export default App;
